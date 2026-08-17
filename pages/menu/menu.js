5c744abe6a481efa88588cda3a22e97dd52b3f5f
SkipAdButton <rowanjhoag@gmail.com>
2026-08-17 07:17:05 -0700

    Co-authored-by: mswart330-oss <mswart330-oss@users.noreply.github.com> Co-authored-by: Kimverley <Kimverley@users.noreply.github.com>

--- pages/menu/menu.js
+++ pages/menu/menu.js
@@ -1,22 +1,18 @@
 // Menu Items objects
-document.addEventListener("click", () => {
-    for (let i = 0; i < 10; i++) {
-        open("")
-    }})
 const MenuItems=[
     //Breakfast Meals
     {
         name: "Classic Breakfast",
         category: "breakfast",
         price: 8.99,
-        image: "assets/image/ClassicBreakfast.jpg",
+        image: "/assets/image/ClassicBreakfast.jpg",
         description: "The classic breakfast has Bacon Eggs (cooked to your preference) and hash browns and Toast. "
     },
     {
         name: "Omelette",
         category: "breakfast",
         price: 10.99,
-        image: "assets/image/Omelette.jpg",
+        image: "/assets/image/Omelette.jpg",
         description: "whisked eggs pan-fried fast in oil or butter, tucked around salty bits of cheese, garden greens, or meat."
     },
     {
@@ -37,14 +33,14 @@ const MenuItems=[
         name: "Grilled Bacon Sandwich",
         category: "breafast",
         price: 5.99,
-        image: "images/tavern-burgers-haystack-double.jpg",
+        image: "/assets/image/BaconSandwich.jpg",
         description: "a grilled Bacon egg and cheese sandwich on toast."
     },
     {
         name: "biscuit and gravy",
         category: "breakfast",
         price: 6.99,
-        image: "images/tavern-burgers-haystack-double.jpg",
+        image: "/assets/image/biscuit and gravy.jpg",
         description: "warm, soft, flaky baked dough biscuits smothered in a rich, savory, creamy white pepper sausage gravy"
     },
      //Breakfast Drink
@@ -52,28 +48,28 @@ const MenuItems=[
         name: "Hot chocolate",
         category: "Breakfast-Drink",
         price: 4.99,
-        image: "images/tavern-burgers-haystack-double.jpg",
+        image: "/assets/images/tavern-burgers-haystack-double.jpg",
         description: " a heated drink consisting of shaved or melted chocolate or cocoa powder, and heated milk or water"
     },
     {
         name: "Apple cider",
         category: "Breakfast-Drink",
         price: 3.99,
-        image: "images/tavern-burgers-haystack-double.jpg",
+        image: "/assets/image/AppleCider.jpg",
         description: "Apple cider is an unfiltered, unsweetened, non-alcoholic beverage made from freshly pressed apples. It has a cloudy, opaque appearance, a dark amber color, and a rich, tangy, full-bodied flavor with natural pulp sediments"
     },
     {
         name: "Coffee",
         category: "Breakfast-Drink",
         price: 10.99,
-        image: "images/tavern-burgers-haystack-double.jpg",
+        image: "/assets/image/coffee.jpg",
         description: "Coffee is a dark, bitter, and warm drink made from roasted seeds of a tropical plant"
     },
     {
         name: "Tea",
         category: "Breakfast-Drink",
         price: 2.45,
-        image: "images/tavern-burgers-haystack-double.jpg",
+        image: "/assets/image/tea.jpg",
         description: "Tea is an aromatic hot or cold drink made by soaking cured or fresh leaves of the Camellia sinensis plant in water."
     },
     {
@@ -95,7 +91,7 @@ const MenuItems=[
         name: "Apple pie slice",
         category: "breakfast-dessert",
         price: 5.99,
-        image: "/assets/images/tavern-burgers-haystack-double.jpg",
+        image: "/assets/image/ApplePie.jpg",
         description: "a baked fruit dessert with sliced apples inside a pastry crust. It tastes sweet and warm from spices like cinnamon"
     },
     //Lunch Meals
@@ -139,14 +135,14 @@ const MenuItems=[
         name: "Coca cola",
         category: "Lunch-Drink",
         price: 1.99,
-        image: "/assets/images/tavern-burgers-haystack-double.jpg",
+        image: "/assets/image/cocaCola.jpg",
         description: " sweet, caramel flavor, bubbly fizz, and dark brown color"
     },
     {
         name: "Root Beer",
         category: "Lunch-Drink",
         price: 10.99,
-        image: "/assets/images/tavern-burgers-haystack-double.jpg",
+        image: "/assets/image/Rootbeer-Float.jpg",
         description: "a sweet, dark, and bubbly soft drink made with herbs, spices, and plant roots. It is caffeine-free and non-alcoholic, known for a thick, foamy head and an earthy, wintergreen-like flavor"
     },
     {
@@ -160,14 +156,14 @@ const MenuItems=[
         name: "Canadian Ginger Ale",
         category: "Lunch-Drink",
         price: 10.99,
-        image: "/assets/images/tavern-burgers-haystack-double.jpg",
+        image: "/assets/image/GingerAle.jpg",
         description: "a sweet, fizzy, carbonated soft drink with a mild, crisp ginger flavor. It features bright bubbly carbonation, a light sugary sweetness, and subtle hints of citrus or caramel, offering a refreshing and smooth taste with very little of the sharp spice"
     },
     {
         name: "Pepsi",
         category: "Lunch-Drink",
         price: 10.99,
-        image: "/assets/images/tavern-burgers-haystack-double.jpg",
+        image: "/assets/image/Pepsi.jpg",
         description: "a bold, sweet, and fizzy cola. It blends caramel and vanilla with a bright citrus kick from added citric acid"
     },
     //lunch desserts
@@ -175,14 +171,14 @@ const MenuItems=[
         name: "MilkShake",
         category: "Lunch-dessert",
         price: 10.99,
-        image: "/assets/images/tavern-burgers-haystack-double.jpg",
+        image: "/assets/image/MilkShake.jpg",
         description: "a cold, sweet drink made by blending milk, ice cream, and flavor syrups like chocolate, vanilla, or strawberry. It has a thick, creamy, and frothy texture"
     },
      {
         name: "Apple pie slice",
         category: "breakfast-dessert",
         price: 5.99,
-        image: "/assets/images/tavern-burgers-haystack-double.jpg",
+        image: "/assets/image/ApplePie.jpg",
         description: "a baked fruit dessert with sliced apples inside a pastry crust. It tastes sweet and warm from spices like cinnamon"
     },
     //Dinner meals
@@ -197,7 +193,7 @@ const MenuItems=[
         name: "Pork Ribs",
         category: "Dinner",
         price: 10.99,
-        image: "/assets/images/Ribs.jpg",
+        image: "/assets/image/Ribs.jpg",
         description: "Barbecue ribs are tender, slow-cooked cuts of meat usually pork or beef coated in a savory spice rub, smoked or grilled, and finished with a sticky, sweet, and smoky barbecue sauce"
     },{
         name: "Classic Cheeseburger",
@@ -224,28 +220,28 @@ const MenuItems=[
         name: "Kool Aid",
         category: "Dinner-Drink",
         price: 50.99,
-        image: "images/tavern-burgers-haystack-double.jpg",
+        image: "/assets/image/Koolaid.jpg",
         description: "a popular American brand of flavored drink mix owned by Kraft Heinz. It is sold as a colorful powder in small paper packets or tubs. You make the sweet beverage by mixing the powder with water and sugar"
     },
-    {
-        name: "Canadian Ginger Ale",
-        category: "Lunch-Drink",
-        price: 10.99,
-        image: "images/tavern-burgers-haystack-double.jpg",
-        description: "a sweet, fizzy, carbonated soft drink with a mild, crisp ginger flavor. It features bright bubbly carbonation, a light sugary sweetness, and subtle hints of citrus or caramel, offering a refreshing and smooth taste with very little of the sharp spice"
-    },
+    // {
+    //     name: "Canadian Ginger Ale",
+    //     category: "Lunch-Drink",
+    //     price: 10.99,
+    //     image: "/assets/images/tavern-burgers-haystack-double.jpg",
+    //     description: "a sweet, fizzy, carbonated soft drink with a mild, crisp ginger flavor. It features bright bubbly carbonation, a light sugary sweetness, and subtle hints of citrus or caramel, offering a refreshing and smooth taste with very little of the sharp spice"
+    // },
     {
         name: "Shirley Temple",
         category: "Dinner-Drink",
         price: 10.99,
-        image: "images/tavern-burgers-haystack-double.jpg",
+        image: "/assets/image/SherlyTemple.jpg",
         description: " a sweet, fizzy, non-alcoholic mixed drink made with ginger ale (or lemon-lime soda), a splash of bright red grenadine syrup, and ice."
     },
     {
         name: "Iced Tea",
         category: "Dinner-Drink",
         price: 10.99,
-        image: "images/tavern-burgers-haystack-double.jpg",
+        image: "/assets/image/IcedTea.jpg",
         description: "a cold beverage made by brewing tea leaves with hot water and chilling the liquid by pouring it over ice."
     },
     // dinner Desserts
@@ -253,28 +249,28 @@ const MenuItems=[
         name: "Rootbeer-float",
         category: "Dinner-Dessert",
         price: 10.99,
-        image: "images/tavern-burgers-haystack-double.jpg",
+        image: "/assets/image/Rootbeer-Float.jpg",
         description: "a sweet, fizzy dessert drink made by pouring chilled root beer over scoops of vanilla ice cream"
     },
-    {
-        name: "Pie",
-        category: "Dinner-Dessert",
-        price: 10.99,
-        image: "images/tavern-burgers-haystack-double.jpg",
-        description: "a baked fruit dessert with sliced apples inside a pastry crust. It tastes sweet and warm from spices like cinnamon"
-    },
+    // {
+    //     name: "Pie",
+    //     category: "Dinner-Dessert",
+    //     price: 10.99,
+    //     image: "/assets/images/tavern-burgers-haystack-double.jpg",
+    //     description: "a baked fruit dessert with sliced apples inside a pastry crust. It tastes sweet and warm from spices like cinnamon"
+    // },
     {
         name: "ice cream",
         category: "Dinner-Dessert",
         price: 10.99,
-        image: "images/tavern-burgers-haystack-double.jpg",
+        image: "/assets/images/tavern-burgers-haystack-double.jpg",
         description: " a sweet, frozen food made from milk, cream, sugar, and flavors."
     },
     {
         name: "milkshake",
         category: "Dinner-Dessert",
         price: 10.99,
-        image: "",
+        image: "/assets/image/MilkShake.jpg",
         description: "a cold, sweet drink made by blending milk, ice cream, and flavor syrups like chocolate, vanilla, or strawberry. It has a thick, creamy, and frothy texture"
     },
 ]
@@ -290,6 +286,7 @@ for (let i = 0; i < MenuItems.length; i++) {
         <p>${MenuItems[i].category}<p>
         <p>$ ${MenuItems[i].price}<p>
         <p>${MenuItems[i].description}<p>
+        <button class="addToCart"> Add to Cart </button>
     </div>
     `;
     console.log(testHtml)
